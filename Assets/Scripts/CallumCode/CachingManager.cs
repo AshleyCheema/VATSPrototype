816542c0bc4a6057b6e5fc0611c89342dbdfa0ab
Ashley <ashleyycheemaa@hotmail.com>
2020-04-09 13:12:28 +0100

    Added CallumCode

---
+++ Assets/Scripts/CallumCode/CachingManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CallumCode
+{
+    public sealed class CachingManager
+    {
+        Dictionary<object, int> m_IDCache = new Dictionary<object, int>();
+
+        // Caching method takes the string value of an enum and tries to get a value from Unity
+        public void Init(Type enumeratedValues, Func<string, int> cachingMethod)
+        {
+            Array values = enumeratedValues.GetEnumValues();
+            foreach (var enumVal in values)
+            {
+                // Mapping the enum name to parameter hash
+                m_IDCache[enumVal] = cachingMethod.Invoke(enumVal.ToString());
+            }
+        }
+
+        public int this[Enum enumVal]
+        {
+            get
+            {
+                return m_IDCache[enumVal];
+            }
+
+            set
+            {
+                m_IDCache[enumVal] = value;
+            }
+        }
+    }
+
+    /*
+     * This caching manager will relate the underlying value of an 
+     * enum to a value that the cachingMethod can handle using the 
+     * string value of that enum. For example:
+     * 
+     * My shader has a value called 'FillProgress'. To set this value
+     * my enum will look like this:
+     * 
+     * enum ShaderValues
+     * {
+     *     FillProgress,
+     *     Alpha
+     *     Green
+     * }
+     * 
+     * The typed definitions of the enum must match the variable name
+     * including case.
+     * 
+     * Init is then called using this enums type: 
+     *      Init(typeof(ShaderValues), cachingMethod) 
+     * This caching method is likely to be something like Shader.PropertyToID
+     * or Animator.StringToHash
+     * 
+     * To use this ID later, access with [] and use the enum value for the 
+     * property you're trying to get the index for. For example:
+     * 
+     * m_animator.SetTrigger(m_cache[AnimatorValues.OnShoot])
+     * opposed to...
+     * m_animator.SetTrigger("OnShoot");
+     */
+
+}
---
+++ Assets/Scripts/CallumCode/CharacterZoneHighlighter.cs
@@ -0,0 +1,151 @@
+﻿#define Kara_Debug
+
+using System;
+using UnityEngine;
+
+
+namespace CallumCode
+{
+    public class CharacterZoneHighlighter : MonoBehaviour
+    {
+        [SerializeField] MeshRenderer meshRenderer              = default;
+        [SerializeField] Color HighlightColour                  = default;
+        [Header("Zone Values")]
+        [SerializeField][Range(0, 255)] float HeadTexValue      = default;
+        [SerializeField][Range(0, 255)] float ArmsTexValue      = default;
+        [SerializeField][Range(0, 255)] float TorsoTexValue     = default;
+        [SerializeField][Range(0, 255)] float LegsTexValue      = default;
+        [Header("Pulse settings")]
+        [SerializeField] bool ShouldPulse                       = default;
+        [SerializeField] float PulseRangeMin                    = default;
+        [SerializeField] float PulseRangeMax                    = default;
+        [SerializeField] float PulseSpeed                       = default;
+
+        CachingManager m_cachingManager                         = default;
+        Material m_VATsMaterial                                 = default;
+
+        const float m_precisionAllowence = 0.01f;
+        BodyZone m_selectedZone = BodyZone.None;
+
+
+        public void Init()
+        {
+            m_VATsMaterial = meshRenderer.material;
+
+            m_cachingManager = new CachingManager();
+            m_cachingManager.Init(typeof(ShaderValues), Shader.PropertyToID);
+
+            m_VATsMaterial.SetColor(m_cachingManager[ShaderValues._HighlightColor], HighlightColour);
+        }
+
+        public void Process()
+        {
+            if (ShouldPulse)
+            {
+                float brightness = Mathf.Lerp(PulseRangeMin, PulseRangeMax, ((Mathf.Sin(Time.timeSinceLevelLoad * PulseSpeed) + 1) * 0.5f));
+                m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._HighlightBrightness], brightness);
+            }
+        }
+
+        public void HighlightZone(BodyZone bodyZone)
+        {
+            m_selectedZone = bodyZone;
+
+            switch (bodyZone)
+            {
+                case BodyZone.Head:
+                    SetSelectedZone(HeadTexValue);
+                    break;
+                case BodyZone.Arms:
+                    SetSelectedZone(ArmsTexValue);
+                    break;
+                case BodyZone.Torso:
+                    SetSelectedZone(TorsoTexValue);
+                    break;
+                case BodyZone.Legs:
+                    SetSelectedZone(LegsTexValue);
+                    break;
+                case BodyZone.None:
+                    m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMin], 0.0f);
+                    m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMax], 0.0f);
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        public void TogglePulse(bool shouldPulse)
+        {
+            ShouldPulse = shouldPulse;
+            m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._HighlightBrightness], 1.0f);
+        }
+
+
+        private void SetSelectedZone(float zoneID)
+        {
+            float conv = zoneID / 256;
+
+            m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMin], conv - m_precisionAllowence);
+            m_VATsMaterial.SetFloat(m_cachingManager[ShaderValues._SelectedZoneMax], conv + m_precisionAllowence);
+        }
+
+
+        public enum BodyZone
+        {
+            Head = 1,
+            Arms,
+            Torso,
+            Legs,
+            None
+        }
+
+        private enum ShaderValues
+        {
+            _SelectedZoneMin,
+            _SelectedZoneMax,
+            _HighlightColor,
+            _HighlightBrightness
+        }
+
+
+
+
+        private void Update()
+        {
+            Process();
+        }
+
+        private void Awake()
+        {
+            Init();
+        }
+
+#if Kara_Debug
+
+        Array debug_EnumVals = (typeof(BodyZone)).GetEnumValues();
+
+        private void OnGUI()
+        {
+            TogglePulse(GUILayout.Toggle(ShouldPulse, "Pulse"));
+
+            foreach (var enumVal in debug_EnumVals)
+            {
+                BodyZone cur = (BodyZone)enumVal;
+
+                if (cur == m_selectedZone)
+                {
+                    GUI.color = Color.green;
+                }
+                if (GUILayout.Button($"Select {enumVal.ToString()}"))
+                {
+                    HighlightZone(cur);
+                }
+                GUI.color = Color.white;
+            }
+        }
+
+#endif
+    }
+
+}
